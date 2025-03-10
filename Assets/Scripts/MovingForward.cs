d72386c4e934b252b9d09b0ed82d9fcd381f22f4
Alex <bezproblemm@gmail.com>
2025-03-10 16:21:57 -0300

    Initial commit

---
+++ Assets/Scripts/MovingForward.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MovingForward : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _distance;
+
+    private Vector3 _startPosition;
+
+    private int _direction = 1;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _direction * _speed * Time.deltaTime);
+
+        if (Vector3.Distance(_startPosition, transform.position) >= _distance)
+        {
+            _direction *= -1;
+            _startPosition = transform.position;
+        }
+    }
+}
