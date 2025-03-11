f0121e9c80cdbd5efcc3c8a9b07ec3fa4eb86d32
Alex <bezproblemm@gmail.com>
2025-03-11 11:34:32 -0300

    Удалил закомментированную часть кода в MoverForward.cs

--- Assets/Scripts/MoverForward.cs
+++ Assets/Scripts/MoverForward.cs
@@ -4,13 +4,6 @@ public class MoverForward : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
-    /*private Vector3 _startPosition;
-
-    private void Start()
-    {
-        _startPosition = transform.position;
-    }
-*/
     private void Update()
     {
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
