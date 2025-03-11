dfb1558d83002826203a6d4babdda9c13fde4f45
Alex <bezproblemm@gmail.com>
2025-03-11 11:31:48 -0300

    Обновил скрипты, выставил новые значения в инспекторе
    1. Изменил скрипт Rotator.cs, чтобы объект двигался только вперед
    2. Изменил скрипт Scaler.cs. Удалил часть кода, которая не несла полезный функционал
    3. В скрипте Rotator.cs поменял название поля, чтобы оно не совпадало с названием скрипта
    4. Настроил значения полей в инспекторе

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _speed;
 
     private void Update()
     {
-        transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0);
+        transform.Rotate(0, _speed * Time.deltaTime, 0);
     }
 }
--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -3,24 +3,12 @@ using UnityEngine;
 public class Scaler : MonoBehaviour
 {
     [SerializeField] private float _scaleSpeed;
-    [SerializeField] private float _maxScaleSize;
-    [SerializeField] private float _minScaleSize;
 
-    private Vector3 _originalScaleSize;
-    
     private float _scaleDirection = 1f;
-
-    private void Start()
-    {
-        _originalScaleSize = transform.localScale;
-    }
 
     private void Update()
     {
         float scaleChange = _scaleSpeed * Time.deltaTime * _scaleDirection;
         transform.localScale += new Vector3(scaleChange, scaleChange, scaleChange);
-
-        if (transform.localScale.x >= _maxScaleSize || transform.localScale.x <= _minScaleSize)
-            _scaleDirection *= -1;
     }
 }
