223ddcd06c54dad4fb816611f2fbd5ea34873cd0
Alex <bezproblemm@gmail.com>
2025-03-11 11:40:47 -0300

    Сократил скрипт Scaler.cs

--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -4,11 +4,8 @@ public class Scaler : MonoBehaviour
 {
     [SerializeField] private float _scaleSpeed;
 
-    private float _scaleDirection = 1f;
-
     private void Update()
     {
-        float scaleChange = _scaleSpeed * Time.deltaTime * _scaleDirection;
-        transform.localScale += new Vector3(scaleChange, scaleChange, scaleChange);
+        transform.localScale += Vector3.one * _scaleSpeed * Time.deltaTime;
     }
 }
